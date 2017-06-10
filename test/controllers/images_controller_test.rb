c408c1d5c5d8df38fd5c39963ead5786d8e5bd95
Joe-noh <goflb.jh@gmail.com>
2017-06-11 01:18:21 +0900

    remove tests

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -3,14 +3,4 @@ require 'test_helper'
 class ImagesControllerTest < ActionDispatch::IntegrationTest
   setup do
   end
-
-  test "post image url and process mode" do
-    params = {
-      image_url: "http://lorempixel.com/400/200/sports/",
-      mode: "all",
-    }
-
-    post process_image_url, params: params, as: :json
-    assert_response :success
-  end
 end
