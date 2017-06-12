996b6a2960891f74f5b56fc267f33e895b816213
Joe-noh <goflb.jh@gmail.com>
2017-06-13 00:49:02 +0900

    set rails_env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+set :rails_env, "production"
 server "onamae", user: "joe", roles: %w[app worker]
 
 # role-based syntax
