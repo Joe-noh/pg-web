7f2b1a19ad53cff51b90edd71c01592d56328f5f
Joe-noh <goflb.jh@gmail.com>
2017-06-13 00:59:00 +0900

    remove rbenv exec

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.3'
 
 set :rbenv_path, "/home/joe/.anyenv/envs/rbenv"
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} "
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
 
